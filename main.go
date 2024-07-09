4bed374f24713bdb7c436ba23a26e68386c032cc
Thim Cederlund <thim@cederlund.de>
2024-07-09 21:37:42 +0200

    add a -o flag for generating passwords once

--- main.go
+++ main.go
@@ -18,9 +18,11 @@ import (
 
 var (
 	providers = make(map[string]string)
-	secrets   = flag.String("f", "", "file path to a secrets file (if any)")
-	datefmt   = flag.String("D", "15:04:06", "date format of the next key generation")
-	digits    = flag.Int("d", 6, "amount of digits")
+	secrets   = flag.String("f", "", "file path to the secrets file")
+	datefmt   = flag.String("D", "15:04:06", "date format of the next generation")
+	digits    = flag.Int("d", 6, "amount of digits in the passwords")
+	interval  = flag.Int("i", 30, "delay (in seconds) between each generation")
+	once      = flag.Bool("o", false, "generate passwords once")
 )
 
 // TOTP generates a time-based one-time password (TOTP).
@@ -38,7 +40,7 @@ func TOTP(when time.Time, key []byte, interval time.Duration) (string, error) {
 		mac    = hash.Sum(nil)
 		offset = mac[len(mac)-1] & 0xF
 	)
-	return fmt.Sprintf("%0*d", *digits, uint(binary.BigEndian.Uint32(mac[offset:offset+4])&0x7FFFFFFF) % uint(math.Pow10(*digits))), nil
+	return fmt.Sprintf("%0*d", *digits, uint(binary.BigEndian.Uint32(mac[offset:offset+4])&0x7FFFFFFF)%uint(math.Pow10(*digits))), nil
 }
 
 func parse(f *os.File) error {
@@ -62,11 +64,9 @@ func parse(f *os.File) error {
 }
 
 func usage() {
-	prog := filepath.Base(os.Args[0])
-	fmt.Fprintf(os.Stderr, "Usage: %s [-f file] [-d digits] [-D date format]\n\n", prog)
-	fmt.Fprintf(os.Stderr, "If the -f flag is not been specified, %s will read from standard input.\n", prog)
-	fmt.Fprintf(os.Stderr, "%s expects data that is TAB separated and it needs contain two fields,\n", prog)
-	fmt.Fprintf(os.Stderr, "the first field is a display name for the service, the other is the secret key.\n\n")
+	fmt.Fprintf(os.Stderr, "%s will read from standard input if -f is not specified.\n", filepath.Base(os.Args[0]))
+	fmt.Fprintf(os.Stderr, "File is expected to be tab separated containing the display\nname and the secret itself.\n\n")
+	flag.PrintDefaults()
 	os.Exit(1)
 }
 
@@ -88,23 +88,26 @@ func main() {
 		os.Exit(1)
 	}
 	var (
-		dur = time.Second * 30
+		dur = time.Second * time.Duration(*interval)
 		t   = time.NewTicker(dur)
 	)
 	for ; true; <-t.C {
 		fmt.Printf("%s - Next in %s\n", time.Now().Format(*datefmt), dur)
-		for n, k := range providers {
-			decoded, err := base32.StdEncoding.DecodeString(string(k))
+		for name, key := range providers {
+			decoded, err := base32.StdEncoding.DecodeString(string(key))
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "decoding failed: %q (%s)\n", err, n)
-				decoded = []byte(k)
+				fmt.Fprintf(os.Stderr, "decoding failed: %q (%s)\n", err, name)
+				decoded = []byte(key)
 			}
-			secret, err := TOTP(time.Now(), decoded, time.Duration(time.Second*30))
+			secret, err := TOTP(time.Now(), decoded, dur)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "totp: %q", err)
 				continue
 			}
-			fmt.Printf("%-25s %s\n", n, secret)
+			fmt.Printf("%-25s %s\n", name, secret)
+		}
+		if *once {
+			break
 		}
 	}
 }
